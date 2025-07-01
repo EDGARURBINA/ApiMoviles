81b0fee03476ac061a66297fbacc8ca996ab2455
Insano <insano@gmail.com>
2025-06-30 22:48:56 -0600

    SinToken

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,6 +9,6 @@ router.post("/signin",authCtrl.signin);
 router.post("/signup", upload.single('image'), authCtrl.signup);
 router.get("/users",authCtrl.getAllUsers);
 router.delete("/users/:id",authCtrl.deleteUser);
-router.put("/users/:id", verifyToken, upload.single('image'), authCtrl.updateUser);
+router.put("/users/:id", upload.single('image'), authCtrl.updateUser);
 
 export default router;
